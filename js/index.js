50455875c7e0c827e0d07b94d4e7434fe3d5eec7
Maxim <maxim13danilov@gmail.com>
2021-08-02 17:59:38 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,61 @@
+document.addEventListener('DOMContentLoaded', function() {
+  setTimeout(animationTiming, 1);
+});
+
+function animationTiming() {
+  blockAnimation();
+  setTimeout(circleAnimation, 1800);
+}
+
+function blockAnimation() {
+  let blockList = document.querySelectorAll('.block');
+  let translateValue = [
+    ["-50vw", "-50vh"],
+    ["50vw", "-50vh"],
+    ["-50vw", "50vh"],
+    ["50vw", "50vh"],
+  ];
+
+  for (let i = 0; i < 4; i++) {
+    blockList[i].style.transform = "translate(" + translateValue[i][0] + ", " + translateValue[i][1] + ")";
+  }
+
+  setTimeout( function(){
+    document.querySelectorAll('.block').forEach(elem => {
+      elem.classList.toggle("d-none");
+      elem.style.transform = "none";
+    });
+  }, 2000);
+}
+
+function circleAnimation() {
+  let circleBtn = document.querySelector('.circle');
+  circleBtn.classList.toggle("circleBtnAnimationStart");
+
+  setTimeout(function(){ 
+    document.querySelector('.circle').disabled = false;
+  }, 3000);
+}
+
+function formAnimation() {
+  let circleBtn = document.querySelector('.circle');
+  circleBtn.classList.toggle("circleBtnAnimationStart");
+  circleBtn.disabled = true;
+
+  let form = document.querySelector('.form');
+  form.classList.toggle("formAnimation");
+}
+
+function resetAnimation() {
+  document.querySelectorAll('.block').forEach(elem => {
+    elem.classList.toggle("d-none");
+  });
+
+  setTimeout(animationTiming, 1);
+}
+
+function fillSelect() {
+  var carObj = JSON.parse("bd.json");
+  console.log(carObj);
+  let markSelect = document.querySelector('.mark');
+}
