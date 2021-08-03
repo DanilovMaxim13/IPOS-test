55e1e09cba43ac64f27b3a2f73c5ec0ce1773f1b
Maxim <maxim13danilov@gmail.com>
2021-08-03 11:18:12 +0500

    Edit logic form

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
   setTimeout(animationTiming, 1);
-  fillSelect();
 });
 
 function animationTiming() {
@@ -45,6 +44,7 @@ function formAnimation() {
 
   let form = document.querySelector('.form');
   form.classList.toggle("formAnimation");
+  fillSelect();
 }
 
 function resetAnimation() {
@@ -54,9 +54,72 @@ function resetAnimation() {
 
   setTimeout(animationTiming, 1);
 }
-
+let carList = [];
 function fillSelect() {
-  let carList = $.getJSON( "https://danilovmaxim13.github.io/IPOS-test/bd.json", function(data){
-            console.log(data);
-      })
+  var xhr = new XMLHttpRequest ();
+  xhr.open("GET", "https://danilovmaxim13.github.io/IPOS-test/bd.json", true);
+  xhr.onload = function() {
+    carList = JSON.parse(xhr.responseText);
+    console.log(carList);
+
+    let marks = new Set();
+    let models = new Set();
+    let years = new Set();
+
+    carList.forEach(function(item) {
+      marks.add(item["mark"]);
+      models.add(item["model"]);
+      years.add(item["year"]);
+    });
+
+    updateOption("mark", marks);
+    updateOption("model", models);
+    updateOption("year", years);
+  }
+
+  xhr.send();
+}
+
+function updateOption(className, optionList) {
+  let select = document.querySelector("." + className); 
+  select.innerHTML = "<option disabled hidden selected value = \"\"></option>";
+
+  optionList.forEach(item => {
+    let option = document.createElement("option");
+    if (optionList.size === 1) {
+      select.innerHTML = "";
+      option.selected = true;
+    }
+    option.textContent = item;
+    option.value = item;
+    select.appendChild(option);
+  })
+}
+
+function changeSelect() {
+  let marks = new Set();
+  let models = new Set();
+  let years = new Set();
+
+  let selectMark = document.querySelector(".mark").value;
+  let selectModel = document.querySelector(".model").value;
+  let selectYear = document.querySelector(".year").value;
+
+  if (selectMark && selectModel && selectYear) {
+    console.log("Появление кнопки");
+  }
+
+  carList.forEach(function(item) {
+    if ((item["mark"] === selectMark || !selectMark) &&
+        (item["model"] === selectModel || !selectModel) &&
+        (item["year"] === selectYear || !selectYear)) {       
+          marks.add(item["mark"]);
+          models.add(item["model"]);
+          years.add(item["year"]);
+        }
+  });
+
+  updateOption("mark", marks);
+  updateOption("model", models);
+  updateOption("year", years);
 }
